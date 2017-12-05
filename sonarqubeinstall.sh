cc94ede893fa7ac46d177e6080d2e531bbbbdcaf
Devin St. Clair <Devin82m@users.noreply.github.com>
2017-12-04 19:02:37 -0600

    2017-12-04

--- sonarqubeinstall.sh
+++ sonarqubeinstall.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 # Updates
-sudo yum update -y
+yum update -y
 
 # Wait for all updates to install
 sleep 2m
 
 # Install the correct JDK
-sudo yum install java-1.8.0-openjdk java-1.8.0-openjdk-devel -y
+yum install java-1.8.0-openjdk java-1.8.0-openjdk-devel -y
 
 # Install SonarQube
-sudo wget -O /etc/yum.repos.d/sonar.repo http://downloads.sourceforge.net/project/sonar-pkg/rpm/sonar.repo
-sudo yum install sonar
-sudo service start sonar
-sudo chkconfig sonar
-
-
-jdk1.8.0_73/bin/java
+wget -O /etc/yum.repos.d/sonar.repo http://downloads.sourceforge.net/project/sonar-pkg/rpm/sonar.repo
+yum install sonar
+service start sonar
+chkconfig sonar
